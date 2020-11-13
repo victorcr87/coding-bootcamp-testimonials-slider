ce1f81424e6e8e46ca345050f162f9e12e797681
Víctor C.R <victorcr87@gmail.com>
2020-11-13 12:08:19 +0100

    Added mobile version

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const prevBtn = document.querySelector(".btn__prev");
 const nextBtn = document.querySelector(".btn__next");
 const sliders = document.querySelectorAll(".slider");
 
-nextBtn.addEventListener("click", () => {
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+document.addEventListener("keydown", handleKeydown);
+
+function nextSlide() {
   let currentSlider = document.querySelector(".active");
   let currentIndex = Number(currentSlider.classList[1].split(["-"])[1]);
   console.log(sliders.length);
@@ -17,8 +21,9 @@ nextBtn.addEventListener("click", () => {
     currentSlider = document.querySelector(".slider-" + currentIndex);
     currentSlider.classList.add("active");
   }
-});
-prevBtn.addEventListener("click", () => {
+}
+
+function prevSlide() {
   let currentSlider = document.querySelector(".active");
   let currentIndex = Number(currentSlider.classList[1].split(["-"])[1]);
   console.log(sliders.length);
@@ -33,4 +38,13 @@ prevBtn.addEventListener("click", () => {
     currentSlider = document.querySelector(".slider-" + currentIndex);
     currentSlider.classList.add("active");
   }
-});
+}
+
+function handleKeydown(event) {
+  console.log(event.key);
+  if (event.key === "ArrowRight") {
+    nextSlide();
+  } else if (event.key === "ArrowLeft") {
+    prevSlide();
+  }
+}
